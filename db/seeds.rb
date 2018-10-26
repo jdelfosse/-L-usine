cbc598974f991c224a52933460873f489e1cd0f9
jerome.delfosse <jerome.delfosse.78@gmail.com>
2018-10-26 02:51:05 +0200

    maj seed

--- db/seeds.rb
+++ db/seeds.rb
@@ -5,3 +5,11 @@
 #
 #   movies = Movie.create([{ name: 'Star Wars' }, { name: 'Lord of the Rings' }])
 #   Character.create(name: 'Luke', movie: movies.first)
+
+10.times do |index|
+  assembly = Assembly.create!(name: "Nom#{index}")
+end
+
+10.times do |index|
+  part = Part.create!(part_number: "Nom de la partie#{index}")
+end
